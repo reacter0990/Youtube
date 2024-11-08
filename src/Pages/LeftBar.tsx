65b8c41aa25496d9fae9bee30808df25262d1b29
programmscripter <27msi2010@gmail.com>
2024-11-08 11:41:29 +0400

    first commit

--- src/Pages/LeftBar.tsx
+++ src/Pages/LeftBar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Box,
   List,
@@ -17,9 +17,25 @@ import { SiYoutubeshorts } from "react-icons/si";
 import { MdOutlineSubscriptions } from "react-icons/md";
 
 const LeftBar = () => {
+  const [isFocused_1, setIsFocused_1] = useState(false);
+  const [isFocused_2, setIsFocused_2] = useState(false);
+  const [isFocused_3, setIsFocused_3] = useState(false);
+  const [isFocused_4, setIsFocused_4] = useState(false);
+  const [isFocused_5, setIsFocused_5] = useState(false);
   return (
     <Box width={240}>
-      <List sx={{ pt: 2, pl: 2, pr: 2, pb: 0, height: "100%" }}>
+      <List
+        sx={{
+          pt: 2,
+          pl: 2,
+          pr: 2,
+          pb: 0,
+          height: "100%",
+          display: "grid",
+          gridTemplateColumns: "auto",
+          rowGap: "3px",
+        }}
+      >
         <ListItem sx={{ p: 0 }}>
           <ListItemButton
             sx={{
@@ -48,6 +64,11 @@ const LeftBar = () => {
               pl: 1.5,
               height: 40,
             }}
+            onMouseEnter={() => setIsFocused_1(true)}
+            onMouseLeave={() => setIsFocused_1(false)}
+            style={{
+              backgroundColor: isFocused_1 ? "#212121" : "transparent",
+            }}
           >
             <ListItemIcon sx={{ minWidth: 50 }}>
               <SiYoutubeshorts color="#fff" fontSize={22} />
@@ -62,9 +83,15 @@ const LeftBar = () => {
         <ListItem sx={{ p: 0 }}>
           <ListItemButton
             sx={{
+              borderRadius: 3,
               pr: 2,
               pl: 1.5,
               height: 40,
+            }}
+            onMouseEnter={() => setIsFocused_2(true)}
+            onMouseLeave={() => setIsFocused_2(false)}
+            style={{
+              backgroundColor: isFocused_2 ? "#212121" : "transparent",
             }}
           >
             <ListItemIcon sx={{ minWidth: 50 }}>
@@ -95,11 +122,15 @@ const LeftBar = () => {
         <ListItem sx={{ p: 0 }}>
           <ListItemButton
             sx={{
-              bgcolor: "#212121",
               borderRadius: 3,
               pr: 2,
               pl: 1.5,
               height: 40,
+            }}
+            onMouseEnter={() => setIsFocused_3(true)}
+            onMouseLeave={() => setIsFocused_3(false)}
+            style={{
+              backgroundColor: isFocused_3 ? "#212121" : "transparent",
             }}
           >
             <ListItemIcon sx={{ minWidth: 50 }}>
@@ -120,6 +151,12 @@ const LeftBar = () => {
               pl: 1.5,
               height: 40,
             }}
+            onMouseEnter={() => setIsFocused_4(true)}
+            onMouseDown={() => setIsFocused_4(true)}
+            onMouseLeave={() => setIsFocused_4(false)}
+            style={{
+              backgroundColor: isFocused_4 ? "#212121" : "transparent",
+            }}
           >
             <ListItemIcon sx={{ minWidth: 50 }}>
               <CgPlayList
@@ -138,9 +175,15 @@ const LeftBar = () => {
         <ListItem sx={{ p: 0 }}>
           <ListItemButton
             sx={{
+              borderRadius: 3,
               pr: 2,
               pl: 1.5,
               height: 40,
+            }}
+            onMouseEnter={() => setIsFocused_5(true)}
+            onMouseLeave={() => setIsFocused_5(false)}
+            style={{
+              backgroundColor: isFocused_5 ? "#212121" : "transparent",
             }}
           >
             <ListItemIcon sx={{ minWidth: 50 }}>
