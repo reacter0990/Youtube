311b03184956bbbff97747c446f8875ab5c4ef7e
Chingiz <c.topciev@gmail.com>
2024-11-15 15:42:28 +0400

    ..................!!!!

--- client/src/components/ThemeBlur.tsx
+++ client/src/components/ThemeBlur.tsx
@@ -1,17 +1,17 @@
-import { Box } from '@mui/material'
+import { Box } from "@mui/material";
 const ThemeBlur = () => {
   return (
-    <Box position={'absolute'} 
-     top={0} 
-     left={0} 
-     right={0} 
-     bottom={0}
-     bgcolor={'rgba(0,0,0,.1)'}
-     sx={{ backdropFilter: 'blur(5px)' }}
-     >
-        Blur
+    <Box
+      position={"absolute"}
+      top={0}
+      left={0}
+      right={0}
+      bottom={0}
+      bgcolor={"rgba(0,0,0,.1)"}
+      sx={{ backdropFilter: "blur(5px)" }}>
+      Blur
     </Box>
-  )
-}
+  );
+};
 
-export default ThemeBlur
+export default ThemeBlur;
